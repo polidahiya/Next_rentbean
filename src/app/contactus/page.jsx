2867b2827620f7abf9400c5c5af179225fa4da4f
polidahiya830@gmail.com <polidahiya830@gmail.com>
2024-04-16 17:33:15 +0530

    .

--- src/app/contactus/page.jsx
+++ src/app/contactus/page.jsx
@@ -10,28 +10,26 @@ function page() {
         alt="background"
       />
       <div className="h-[100svh] relative">
-        <div className="blackshadow1  p-[40px] text-center  absolute  h-[60%] aspect-[4/3] bg-white rounded-[20px] top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] ">
+        <div className="blackshadow1  p-[40px] text-center  absolute  w-[90%] max-w-[400px] aspect-[4/3] bg-bg1 flex flex-col items-center justify-center rounded-[20px] top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] ">
           <p className="font-semibold text-[20px]"> We are here to help!</p>
-          <center>
-            <Link
-              href="mailto:letsolvein@gmail.com"
-              className=" flex items-center justify-center gap-[5px] w-full md:w-fit mt-[50px]  text-[14px] pt-[10px] lg:hover:underline  "
+          <Link
+            href="mailto:letsolvein@gmail.com"
+            className=" flex items-center justify-center gap-[5px] w-full md:w-fit  text-[14px] pt-[10px] lg:hover:underline  "
+          >
+            <svg
+              className="fill-theme"
+              data-name="Layer 21"
+              height="24"
+              id="Layer_21"
+              viewBox="0 0 24 24"
+              width="24"
+              xmlns="http://www.w3.org/2000/svg"
             >
-              <svg
-                className="fill-theme"
-                data-name="Layer 21"
-                height="24"
-                id="Layer_21"
-                viewBox="0 0 24 24"
-                width="24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <title />
-                <polygon points="3 12 8.61 14.992 17 8 9 17.455 9 21 12.164 16.887 18 20 21 3 3 12" />
-              </svg>
-              Rentbean.in
-            </Link>
-          </center>
+              <title />
+              <polygon points="3 12 8.61 14.992 17 8 9 17.455 9 21 12.164 16.887 18 20 21 3 3 12" />
+            </svg>
+            Rentbean.in
+          </Link>
           <p className="mt-[20px]">
             Rentbean.in - Plot no. 626 basement, sector 39 Gurgaon Haryana
             122002
